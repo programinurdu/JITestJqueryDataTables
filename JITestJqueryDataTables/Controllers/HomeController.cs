429f777c585b91c1ca5fd91e54634d126f06d087
Admin <eaak2004@hotmail.com>
2019-04-05 11:37:18 +0100

    x

---
+++ JITestJqueryDataTables/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using JITestJqueryDataTables.Models;
+using JITestJqueryDataTables.ViewModels.Home;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JITestJqueryDataTables.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            TestViewModel tvm = new TestViewModel();
+            List<Student> students = tvm.GetAllStudents();
+            return View(students);
+        }
+    }
+}
---
+++ JITestJqueryDataTables/ViewModels/Home/TestViewModel.cs
@@ -0,0 +1,23 @@
+﻿using JITestJqueryDataTables.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JITestJqueryDataTables.ViewModels.Home
+{
+    public class TestViewModel
+    {
+        public List<Student> GetAllStudents()
+        {
+            List<Student> students = new List<Student>();
+
+            using (Test2Context db = new Test2Context())
+            {
+                students = db.Students.ToList();
+            }
+
+            return students;
+        }
+    }
+}

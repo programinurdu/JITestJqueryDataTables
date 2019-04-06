dcacb7a5a554a9755e4b60220bc01a9936fcec4a
Admin <eaak2004@hotmail.com>
2019-04-06 21:03:31 +0100

    Part 2 complete

--- JITestJqueryDataTables/Scripts/Site.js
+++ JITestJqueryDataTables/Scripts/Site.js
@@ -1,3 +1,10 @@
 ﻿$(document).ready(function () {
     $('#jdTable').dataTable();
 });
+
+$(document).ready(function () {
+    $('#desktopjdTable').dataTable({
+        "scrollY": '50vh',
+        "paging": false
+    });
+});
